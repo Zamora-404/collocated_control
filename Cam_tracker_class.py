99afa17ab1ddb3eed50279a5959b635b5741873c
Zamora-404 <zamora@bu.edu>
2025-09-19 21:37:28 -0400

    update

--- Cam_tracker_class.py
+++ Cam_tracker_class.py
@@ -4,12 +4,12 @@ import time
 import os
 
 class Cam_Tracker:
-    def __init__(self):
+    def __init__(self, captureFrame = False):
         # ========================== Settings ========================
         CAM_IDS = [0, 2, 4] # Adjust as needed
         VID_PATH = ['output_cam0.avi','output_cam1.avi','output_cam2.avi',]
         IMAGE_SIZE = (1280, 720)
-        isRos = False
+        self.captureFrame = captureFrame
 
         # ================ Marker Kinematics===============
         # Start Kinematics
@@ -19,9 +19,6 @@ class Cam_Tracker:
         R_b0 = np.array([[0, 0, 1],
                         [-1, 0, 0],
                         [0, -1, 0]])
-        # R_bc = np.array([[ 0.9899,  0.0849, -0.1136],
-        #                 [-0.0778,  0.9948,  0.0657],
-        #                 [ 0.1186, -0.0562,  0.9914]])
 
         # p_b0 = - np.array([0.14205, -0.0026393, -0.0263967 - 0])
         p_b0 = - np.array([0.14186, 0.00327, -0.00993 - base_mark_height])
@@ -30,15 +27,22 @@ class Cam_Tracker:
         
         # mass_h = 6.75   # steel mass height (6.6g)
         ef_h = -6     # end effector height 
-        # ef_h = 0
         scale = 1.3     # marker vector scale factor
-        self.marker_ref_b = np.array([
+        self.marker_ryb_ref_b = np.array([
             [ 0.0   * scale, -6.729 * scale,    ef_h],  # red
             [-5.827 * scale, -3.265 * scale,    ef_h],  # yellow
             [-5.827 * scale,  3.265 * scale,    ef_h],  # blue
             ]) / 1000                                   # convert to meters
 
-        self.marker_ref_0 = self.marker_ref_b @ R_b0
+        self.marker_pov_ref_b = np.array([
+            [ 0.0   * scale, -6.729 * scale,    0],  # pink
+            [-5.827 * scale, -3.265 * scale,    0],  # orange
+            [-5.827 * scale,  3.265 * scale,    0],  # violet
+            ]) / 1000   
+        
+
+        self.marker_ryb_ref_0 = self.marker_ryb_ref_b @ R_b0
+        self.marker_pov_ref_0 = self.marker_pov_ref_b @ R_b0
 
         # print(marker_ref_b)
         # print(marker_ref_0)
@@ -54,41 +58,65 @@ class Cam_Tracker:
         for i, cap in enumerate(self.caps):
             cap.set(cv2.CAP_PROP_FRAME_WIDTH, IMAGE_SIZE[0])
             cap.set(cv2.CAP_PROP_FRAME_HEIGHT, IMAGE_SIZE[1])
-            cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-            # self.outs.append( cv2.VideoWriter( 'output_cam'+str(i)+'.avi', fourcc, 30, IMAGE_SIZE) ) # FIX by adding toggle for recording
+            if self.captureFrame:
+                cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+                self.outs.append( cv2.VideoWriter( 'output_cam'+str(i)+'.avi', 30, IMAGE_SIZE) )
 
         # ========================= Begin time and file recording =========================
         self.time_start = time.time()
         self.time_prev = self.time_start - 0.1
 
-
+        # pose for SBA tip marker
         self.R_tip_b = np.eye(3)
         self.t_tip_b = np.array([0,0,0])
+
+        # pose for wall marker
+        self.R_wall_b = np.eye(3)
+        self.t_wall_b = np.array([0,0,0])
+        
+
+
 
 
     def load_params(self, filename):
         data = np.load(filename)
         return data['K'], data['D'], data['R'], data['T']
 
-    def find_marker_center(self, frame, n=2, min_pixels=200):
+    def find_marker_center(self, frame, c, min_pixels=200):
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        # Color thresholding
-        if n == 0:  # Yellow
-            lower = np.array([13, 40, 80])
+
+        # mask Color thresholding
+        if c.lower() == 'y':  # Yellow
+            lower = np.array([16, 40, 80])
             upper = np.array([56, 255, 255])
             mask = cv2.inRange(hsv, lower, upper)
-        elif n == 1:  # Blue
+        elif c.lower() == 'b':  # Blue
             lower = np.array([100, 120, 80])
             upper = np.array([130, 255, 255])
             mask = cv2.inRange(hsv, lower, upper)
-        else:  # Red (n==2)
+        elif c.lower() == 'r':  # Red
             lower_red1 = np.array([0, 80, 80])
             upper_red1 = np.array([5, 255, 255])
-            lower_red2 = np.array([170, 80, 80])
+            lower_red2 = np.array([176, 80, 80])
             upper_red2 = np.array([179, 255, 255])
             mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
             mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
             mask = cv2.bitwise_or(mask1, mask2)
+        elif c.lower() == 'p': # Pink
+            lower = np.array([159, 75, 160])
+            upper = np.array([176, 200, 255])
+            mask = cv2.inRange(hsv, lower, upper)
+        elif c.lower() == 'o': # Orange
+            lower = np.array([6, 88, 175])
+            upper = np.array([18, 217, 255])
+            mask = cv2.inRange(hsv, lower, upper)
+        elif c.lower() == 'v': # Violet
+            lower = np.array([132, 81, 36])
+            upper = np.array([151, 255, 255])
+            mask = cv2.inRange(hsv, lower, upper)
+        else:
+            print("Invalid marker color string")
+
         # Count matching pixels
         pixel_count = cv2.countNonZero(mask)
 
@@ -210,28 +238,27 @@ class Cam_Tracker:
 
             cv2.polylines(frame, [img_backbone], False, (255, 255, 0), 5)   # Backbone in cyan
         return
-
-    def update(self, rigid_coords = np.zeros((3, 4))):
-        time_now = time.time()
-        t_elapsed = time_now - self.time_start
-        dt = time_now - self.time_prev
-        self.time_prev = time_now
+    
+    def get_pts_2D(self, frames, colors):
+        pts_2D_color = []
         
-        frames = [cap.read()[1] for cap in self.caps]
-        if any(f is None for f in frames):
-            print("Frame capture failed")
-            return
-        
-        pts_r = [self.find_marker_center(f, 2) for f in frames]
-        pts_y = [self.find_marker_center(f, 0) for f in frames]
-        pts_b = [self.find_marker_center(f, 1) for f in frames]
-        pts = [pts_r, pts_y, pts_b]
+        # iterate through each color in colors string
+        for c in colors:
+            pt = []
+            # find the color point in each frame and add to list
+            for f in frames:
+                pt.append( self.find_marker_center(f, c) )
+            
+            # add the list of points to master list 
+            pts_2D_color.append(pt)
 
-        # for i, f in enumerate(frames):
-        #     self.outs[i].write(f)
+        return pts_2D_color
+    
+    def get_pts_3D(self, frames, pts2D_color):
 
+        # check which cameras can view each marker by detecting None inside pts_color
         i_views = [[],[],[]]
-        for j, pts_c in enumerate(pts):
+        for j, pts_c in enumerate(pts2D_color):
             for i, pt in enumerate(pts_c):
                 if pt is not None:
                     cv2.circle(frames[i], tuple(pt.astype(int)), 5, (255, 255, 0), -1)
@@ -240,7 +267,7 @@ class Cam_Tracker:
         # i_views = [0, 1]
         pts3d_base = np.zeros((3,3))
         pts3d_global = np.zeros((3,3))
-        for j, pts_c in enumerate(pts):
+        for j, pts_c in enumerate(pts2D_color):
             if len(i_views[j]) >= 2: # triangualte pt if seen by at least two cams
                 params_seen, pts_seen = [self.params[i] for i in i_views[j] ], [pts_c[i] for i in i_views[j]]
                 origins, directions = [], []
@@ -252,45 +279,71 @@ class Cam_Tracker:
                 pt3d_global = self.triangulate_rays(origins, directions).ravel() 
                 pts3d_global[j,:] = pt3d_global
 
-                # the following transforms the color 3d pts to the base frame
+                # transforms the color 3d pts to the base frame
                 pt3d_base = self.T_base_0 @ np.append(pt3d_global, 1) # transform to base frame
                 pts3d_base[j,:] = pt3d_base[0:3] # update all colored pts
                 
+        return pts3d_base, pts3d_global
+    
 
-        T_tip_0 = np.eye(4)  
-        if pts3d_global is not None: 
-            #  extract tip pose using 3 markers in base frame
-            self.R_tip_b, self.t_tip_b = self.estimate_pose(pts3d_base, self.marker_ref_b)
-            # print(R_tip_b)
-            # print(T_tip_base[0:3,0:3])
+        
 
-            # print('Tip position (base):', t_tip_b*1000)
-            # print('Tip position (global):', T_tip_base[0:3,3]*1000)
+    def update(self, rigid_coords = np.zeros((3, 4))):
+        time_now = time.time()
+        t_elapsed = time_now - self.time_start
+        dt = time_now - self.time_prev
+        self.time_prev = time_now
+        
+        # capture frames from cameras and check if successful
+        frames = [cap.read()[1] for cap in self.caps]
+        if any(f is None for f in frames):
+            print("Frame capture failed")
+            return
+        
+        # record capture to video file
+        if self.captureFrame:
+            for i, f in enumerate(frames):
+                self.outs[i].write(f)
+        
+        # set markers using colors
+        color_markers = ['ryb', 'pov']
+        # color_markers = ['ryb']
+        
+        for cm in color_markers:
+            if cm == 'ryb':
+                marker_ref_b, marker_ref_0 = self.marker_ryb_ref_b, self.marker_ryb_ref_0
+            elif cm == 'pov':
+                marker_ref_b, marker_ref_0 = self.marker_pov_ref_b, self.marker_pov_ref_0
 
+            # gets color points in 2D (pixel coordinates from camera frames)
+            pts_2D = self.get_pts_2D(frames, cm)
 
-            # extract tip pose using 3 markers in camera frame
-            R_tip_0, t_tip_0 = self.estimate_pose(pts3d_global, self.marker_ref_0)
+            # gets color points in 3D (cartesian coordinates in base and global/camera1 frame)
+            pts_3D_base, pts_3D_global = self.get_pts_3D(frames, pts_2D)
+            
 
-            # project tip pose and backbone to camera frame 
-            self.draw_pose(self.params, frames, R_tip_0, t_tip_0, rigid_coords)
-            # print(self.R_tip_b)
-            # print(self.t_tip_b)
+            
+            # Find pose for marker if all colors are dected 
+            if pts_3D_global is not None: 
+                #  extract tip pose using 3 markers in base frame
+                if cm == 'ryb':
+                    self.R_tip_b, self.t_tip_b = self.estimate_pose(pts_3D_base, marker_ref_b)
+                elif cm == 'pov':
+                    self.R_wall_b, self.t_wall_b = self.estimate_pose(pts_3D_base, marker_ref_b)
+                
 
-            # print(R_tip_0)
-            # print(t_tip_0)
+                # extract tip pose using 3 markers in camera frame
+                R_tip_0, t_tip_0 = self.estimate_pose(pts_3D_global, marker_ref_0)
 
-            # project base pose and backbone to camera frame
-            # self.draw_pose(self.params, frames, np.eye(3), np.array([0.0009,0.0178,.1421]), rigid_coords)
-            # print(np.eye(3))
-            # print(np.array([0,0,0]))
-
+                # project tip pose and backbone to camera frame 
+                self.draw_pose(self.params, frames, R_tip_0, t_tip_0, rigid_coords)
 
 
 
                 
-        # cv2.imshow(f"Camera 1", frames[0])
-        for i, f in enumerate(frames):
-            cv2.imshow(f"Camera {i+1}", f)
+        cv2.imshow(f"Camera 1", frames[0])
+        # for i, f in enumerate(frames):
+        #     cv2.imshow(f"Camera {i+1}", f)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             return
@@ -300,11 +353,12 @@ class Cam_Tracker:
     def close(self):
         for i, cap in enumerate(self.caps):
             cap.release()
-            # self.outs[i].release()
+            if self.captureFrame:
+                self.outs[i].release()
         cv2.destroyAllWindows()
 
     def get_pose(self):
-        return (self.t_tip_b, self.R_tip_b )
+        return (self.t_tip_b, self.R_tip_b, self.t_wall_b, self.R_wall_b)
 
 
 def main():
